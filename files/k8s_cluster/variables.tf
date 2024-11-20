84124a4bacafa092ee024eb3b823df269cedc5db
artmur1 <142450410+artmur1@users.noreply.github.com>
2024-11-20 22:48:13 +0300

    Add files via upload

---
+++ files/k8s_cluster/variables.tf
@@ -0,0 +1,42 @@
+###cloud vars
+variable "default_zone_a" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "default_zone_b" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "default_zone_d" {
+  type        = string
+  default     = "ru-central1-d"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "network1"
+  description = "VPC network & subnet name"
+}
+
+variable "vm_resources" { 
+  type         = map(map(number))
+  default      = {
+    nat_res = {
+      cores = 2
+      memory = 4
+      core_fraction = 20
+      disk_size = 20
+    }
+    priv_res = {
+      cores = 2
+      memory=2
+      core_fraction=20
+      disk_size = 20
+    }
+  }
+}
